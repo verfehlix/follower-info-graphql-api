f1611ea2c9d4b2c6edef4524ef7dc9310e14dd7e
Felix M <felix.p.magnus@gmail.com>
2019-02-21 20:15:45 +0100

    removed application.yml (was not working for CORS) and disabled CORS via bean

--- src/main/java/com/verfehlix/followerinfographqlapi/FollowerInfoGraphQLApiApplication.java
+++ src/main/java/com/verfehlix/followerinfographqlapi/FollowerInfoGraphQLApiApplication.java
@@ -2,6 +2,9 @@ package com.verfehlix.followerinfographqlapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FollowerInfoGraphQLApiApplication {
@@ -10,5 +13,17 @@ public class FollowerInfoGraphQLApiApplication {
 		SpringApplication.run(FollowerInfoGraphQLApiApplication.class, args);
 	}
 
+	/**
+	 * Bean for enabling CORS on every pattern (used for developing)
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
 
